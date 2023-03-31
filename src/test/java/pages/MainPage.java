03cf16acba5c91f627dc0acec5fbde57f005ece8
timsBt <tims.bekmat@gmail.com>
2023-04-01 01:05:06 +0400

    Main Page

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,4 +1,40 @@
 package pages;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class MainPage {
+
+    private WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public String pageUrl = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager";
+
+    @FindBy(css = (".btn.btn-lg.tab"))
+    private WebElement addCustomer;
+
+    @FindBy(xpath = "//button[@ng-click =  'showCust()']")
+    private WebElement customers;
+
+    public WebElement getAddCustomer(){
+        return addCustomer;}
+
+    public WebElement getCustomers(){
+        return customers;}
+
+
+    @Step("Нажатие кнопки Add Customer")
+    public void clickAddCustomer(){
+        addCustomer.click();}
+
+    @Step("Нажатие кнопки Customers")
+    public void clickCustomers(){
+        customers.click();}
 }
