e9eb663cf65bd8c903a4943f70aa6a7c8572d315
timsBt <tims.bekmat@gmail.com>
2023-04-01 01:08:51 +0400

    WebDriver

--- src/test/java/utils/WebDriver.java
+++ src/test/java/utils/WebDriver.java
@@ -1,4 +1,13 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebDriver {
+
+    public static ChromeDriver getChromeDriver(){
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver();
+    }
+
 }
